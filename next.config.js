4d7103bf4585a4bc037024ce658e3e9273083e16
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-12-21 00:45:30 +0530

    Fixed style.css 3

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
